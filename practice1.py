3a156c0afe78faf28c33ff8f3a2597b42d786ca8
01ro01 <151019824+01ro01@users.noreply.github.com>
2024-02-09 02:27:21 +0600

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,8 @@
+
+name = "Israt"
+age = 22
+
+print("Her name is "+name)
+print("Israt is", age ,"years old")
+print("She lives in Dhaka")
+print("At the age of", age , "she started to learn python")
---
+++ practice2.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result1 = int(num1) + int(num2)
+print("The result is= " ,result1)
+
+result2 = int(num1) - int(num2)
+print("The result is= " ,result2)
+
+result3 = int(num1) * int(num2)
+print("The result is= " ,result3)
